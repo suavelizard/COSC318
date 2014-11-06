6ce99b900a9e97c7aee8a6aa3d7e1c5b7dc878e2
Nicholas Wilkinson <Kharnaxin@hotmail.com>
2014-11-06 12:16:11 -0800

    Added PositionPacketTest class and method stubs

---
+++ dotWar/src/tests/PositionPacketTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PositionPacketTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @Test
+    public void testSetX() throws Exception {
+
+    }
+
+    @Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @Test
+    public void testSetY() throws Exception {
+
+    }
+}
