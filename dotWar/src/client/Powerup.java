c2d80897229e07c133630647cea06644857d6d2a
Nicholas Wilkinson <Kharnaxin@hotmail.com>
2014-11-20 13:02:46 -0800

    Added PositionPacketTest class and method stubs

--- dotWar/src/client/Powerup.java
+++ dotWar/src/client/Powerup.java
@@ -4,4 +4,17 @@ package client;
  * Created by Zane on 2014-11-06.
  */
 public class Powerup {
+    private Position position;
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Powerup() {
+        this.setPosition(new Position(400,400));
+    }
 }
--- dotWar/src/client/Projectile.java
+++ dotWar/src/client/Projectile.java
@@ -4,4 +4,44 @@ package client;
  * Created by Zane on 2014-11-06.
  */
 public class Projectile {
+    private Position position;
+    private int moveSpeed;
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    private double damage;
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public int getMoveSpeed() {
+        return moveSpeed;
+    }
+
+    public void setMoveSpeed(int moveSpeed) {
+        this.moveSpeed = moveSpeed;
+    }
+
+    public Projectile() {
+        this.setPosition(new Position(400.0,400.0));
+        this.setMoveSpeed(50);
+        this.setDamage(1);
+    }
+
+    public Projectile(Position position, int moveSpeed, double damage) {
+        this.setPosition(position);
+        this.setMoveSpeed(moveSpeed);
+        this.setDamage(damage);
+    }
 }
